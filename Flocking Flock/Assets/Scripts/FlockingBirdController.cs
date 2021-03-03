b230a972cb3ce09981d8bc4df46186784bf18293
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2021-03-03 01:25:54 +0100

    Add new birds creation (with space key) and random precision and wings power to flocking birds

--- Flocking Flock/Assets/Scripts/FlockingBirdController.cs
+++ Flocking Flock/Assets/Scripts/FlockingBirdController.cs
@@ -8,16 +8,28 @@ public class FlockingBirdController : MonoBehaviour
 
     private Rigidbody rb;
 
-    private float wingsPower = 33.0f;
+    private float wingsPower;
+    public float wingsPowerMin = 15.0f;
+    public float wingsPowerMax = 40.0f;
+    public float precision = 20.0f; // in degrees
+
+    private Vector3 precisionEulers;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        wingsPower = Random.Range(wingsPowerMin, wingsPowerMax);
+        precisionEulers = new Vector3(Random.Range(-precision, precision), Random.Range(-precision, precision), Random.Range(-precision, precision));
     }
 
     void FixedUpdate()
     {
         transform.LookAt(targetBird);
+
+        transform.Rotate(precisionEulers); // Applying precision
+
+        Debug.DrawLine(transform.position, transform.position + transform.forward * wingsPower, Color.blue);
 
         rb.AddForce(transform.forward * wingsPower, ForceMode.Acceleration);
     }
